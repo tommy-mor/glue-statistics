6d22620b5446dd353975f30cf85ef8c47b64f24a
Juna Kim <31077956+jk31768@users.noreply.github.com>
2021-02-23 16:08:14 -0500

    Update StatsDataViewer.py

--- StatsDataViewer/StatsDataViewer.py
+++ StatsDataViewer/StatsDataViewer.py
@@ -193,6 +193,40 @@ class SortButton(CheckableTool):
 	def close(self):
 		pass
 
+	
+class StatsViewerState(ViewerState):
+
+
+	expandAll = CallbackProperty(False, docstring='The button which expands or collapses all items')
+	decPlaces = CallbackProperty()
+
+	numNotation = CallbackProperty(False, docstring='The button which changes number notation')
+
+	def __init__(self, *args, **kwargs):
+		super(StatsViewerState, self).__init__(*args, **kwargs)
+		self.expandAll = False
+		self.numNotation = True
+
+
+	def expand_all(self):
+		self.expandAll = not self.expandAll
+
+
+	def change_notation(self):
+		self.numNotation = not self.numNotation
+
+
+class StatsViewerStateWidget(QWidget):
+
+	def __init__(self, viewer_state=None, session=None):
+
+		super(StatsViewerStateWidget, self).__init__()
+
+		self.ui = load_ui('viewer_state.ui', self,
+						  directory=os.path.dirname(__file__))
+
+		self.viewer_state = viewer_state
+		self._connections = autoconnect_callbacks_to_qt(self.viewer_state, self.ui)
 
 
 class StatsDataViewer(DataViewer, HubListener):
