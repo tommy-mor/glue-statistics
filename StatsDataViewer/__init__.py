d2192cfdfe1c7163dc37986be3784bde63f49442
Juna Kim <31077956+jk31768@users.noreply.github.com>
2021-02-23 15:52:15 -0500

    Update __init__.py

--- StatsDataViewer/__init__.py
+++ StatsDataViewer/__init__.py
@@ -1,5 +1,4 @@
 def setup():
-    from StatsDataViewer import StatsDataViewer
+    from .StatsDataViewer import StatsDataViewer
     from glue.config import qt_client
-    #stats = StatsDataViewer(data_collection)
     qt_client.add(StatsDataViewer)
